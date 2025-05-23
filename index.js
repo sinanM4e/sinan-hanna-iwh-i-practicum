fb2fe1c8dea25544f7b039df012ea5bac602abf9
Sinan Hanna <s.hanna@motion4ever>
2025-05-23 17:53:55 +0200

    Implement custom object routes and views for HubSpot practicum

--- index.js
+++ index.js
@@ -1,26 +1,70 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-
+require('dotenv').config();
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+ const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
+
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        const response = await axios.get(`https://api.hubapi.com/crm/v3/objects/${process.env.CUSTOM_OBJECT_TYPE}?properties=name,power,origin`, {
+            headers
+        });
+
+        const data = response.data.results;
+        res.render('homepage', { title: 'Homepage | HubSpot Practicum', objects: data });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Erreur lors de la récupération des objets personnalisés');
+    }
+});
+
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', { title: 'Update Custom Object Form | HubSpot Practicum' });
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    const { name, power, origin } = req.body;
+
+    try {
+        await axios.post(`https://api.hubapi.com/crm/v3/objects/${process.env.CUSTOM_OBJECT_TYPE}`, {
+            properties: {
+                name,
+                power,
+                origin
+            }
+        }, { headers });
+
+        res.redirect('/');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Erreur lors de la création de l’enregistrement');
+    }
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
